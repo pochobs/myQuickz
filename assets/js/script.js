4a7d66e4d0b693f00391baec7701938cc17aa5a2
Pochobs <pochobs@hotmail.com>
2020-06-29 00:56:39 -0500

    pseudo code is added

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+// create header and its functions
+// get element for high scores
+
+// create function to show highscores // will clear page below // will be created by parse JSON  localstorage // a div will hold the list
+//list will be sorted to show higher in the top of the list // and will get information being passed as object to json localStorage.
+
+//two buttons will be created
+
+//create function inside the function to create the object where we pass attibutes information with the click buttom to go back to the quiz
+
+//create function inside the function to create the object where we pass attibutes information with the click buttom to clear score and stay.
+
+// get element timer from header
+
+
+
+//create set interval to the timer//clear interval function timer
+
+
+
+//after creating the function to the header lets create a function to create the questionary array
+//create questionary index
+
+//create questions and answers as and objet
+
+//create and if statement to add the score from answers as an variable to be use and pass to show score function
+
+//create and get the div where questionary will be display
+
+//create and array to show the questionary from its index
+
+//when finish the questionary or time has ended. will pass information to the showscore function,
+
+//show score function puts page in blanck and adds elements to the dom
+
+//add the result from questionary points
+
+//add and input to hold the name
+
+//add a button to save score
+
+// call showhighscore function and pass score data.
+
+//create eventListener to button to call summit and call showighscore function
+
+
+
+//add eventListener when click start function create questionary starts.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
